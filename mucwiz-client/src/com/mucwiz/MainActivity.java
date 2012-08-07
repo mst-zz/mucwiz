e523abc48ada6d010437084ffee88f3cfb5579dc
Andreas Duchen <andreas.duchen@apper.se>
2012-08-07 21:18:15 +0200

    Stuff

--- mucwiz-client/src/com/mucwiz/MainActivity.java
+++ mucwiz-client/src/com/mucwiz/MainActivity.java
@@ -2,15 +2,31 @@ package com.mucwiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (false)//isLoggedIn())
+        if (false)/*isLoggedIn()) */ {
         	setContentView(R.layout.main);
-        else
+        	attachMainMenuListeners();
+        } else {
         	setContentView(R.layout.login);
+	        Button b = (Button)findViewById(R.id.login);
+	        b.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					setContentView(R.layout.main);
+					attachMainMenuListeners();
+				}
+			});
+        }
+    }
+    
+    private void attachMainMenuListeners() {
+    	
     }
 }
