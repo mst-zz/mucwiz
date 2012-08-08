5da6610952af7a5f88ecace8ef9026654ab8cf33
Andreas Duchen <andreas.duchen@apper.se>
2012-08-08 12:02:18 +0200

    Added API call

--- mucwiz-client/src/com/mucwiz/MainActivity.java
+++ mucwiz-client/src/com/mucwiz/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MucwizApi.testCreateQuiz();
+        
         if (false)/*isLoggedIn()) */ {
         	setContentView(R.layout.main);
         	attachMainMenuListeners();
